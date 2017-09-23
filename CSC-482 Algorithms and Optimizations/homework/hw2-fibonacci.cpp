d4116312f56e6889c763ed7f36f51f112eaaff4c
Takeshi <iwatakeshi@gmail.com>
2017-09-23 08:37:22 -0700

    Update

--- CSC-482 Algorithms and Optimizations/homework/hw2-fibonacci.cpp
+++ CSC-482 Algorithms and Optimizations/homework/hw2-fibonacci.cpp
@@ -2,48 +2,54 @@
 #include <map>
 #include <stdlib.h>
 #include <tuple>
+#include <boost/multiprecision/cpp_int.hpp>
+using boost::multiprecision::cpp_int;
+cpp_int limit = (cpp_int(1) << 128) - 1;
+
 // #include <math.h>
-void power(long long A[2][2], int n);
-void multiply(long long A[2][2], long long B[2][2]);
+void power(cpp_int A[2][2], cpp_int n);
+void multiply(cpp_int A[2][2], cpp_int B[2][2]);
 
 // 1. Fibonacci (recursive)
-long long fibonacci(long long n);
+cpp_int fibonacci(cpp_int n);
 // 2. Fibonacci (non-recursive)
-long long looped_fibonacci(int n);
+cpp_int looped_fibonacci(cpp_int n);
 // 3. Fibonacci (memoized)
-long long cached_fibonacci(long long n);
+cpp_int cached_fibonacci(cpp_int n);
 // 4. Fibonacci (matrixed)
-long long matrixed_fibonacci(int n);
+cpp_int matrixed_fibonacci(cpp_int n);
 
-std::tuple<int, int> parse_args(int argc, char * argv[]);
+std::tuple<unsigned long long, int> parse_args(int argc, char * argv[]);
 int parse_flag(char * argv);
 
 int main(int argc, char * argv[]) {
   if (argc >= 2) {
-    int n = 0, flag = 0;
+    cpp_int n = 0;
+    int flag = 0;
     std::tie(n, flag) = parse_args(argc, argv);
     switch(flag) {
-      case 1: std::cout << cached_fibonacci((long long) n) << std::endl; break;
+      case 1: std::cout << cached_fibonacci(n) << std::endl; break;
       case 2: std::cout << looped_fibonacci(n) << std::endl; break;
       case 3: std::cout << matrixed_fibonacci(n) << std::endl; break;
       case 4: {
-        std::cout << "Fibonacci (recursive) " << fibonacci((long long) n) << std::endl;
+        std::cout << "Fibonacci (recursive) " << fibonacci(n) << std::endl;
         std::cout << "Fibonacci (non-recursive) " << looped_fibonacci(n) << std::endl;
-        std::cout << "Fibonacci (memoized) " << cached_fibonacci((long long) n) << std::endl;
+        std::cout << "Fibonacci (memoized) " << cached_fibonacci(n) << std::endl;
         std::cout << "Fibonacci (matrixed) "<< matrixed_fibonacci(n) << std::endl;
         break;
       }
-      default: std::cout << fibonacci((long long) n) << std::endl; break;
+      default: std::cout << fibonacci(n) << std::endl; break;
     }
   }
   return 0;
 }
 
-std::tuple<int, int> parse_args(int argc, char * argv[]) {
-  int n = 0, flag = 0;
+std::tuple<unsigned long long, int> parse_args(int argc, char * argv[]) {
+  unsigned long long n = 0, flag = 0;
+  std::string::size_type sz = 0;
   for (int i = 1; i < argc; i++) {
     int f = parse_flag(argv[i]);
-    int v = atoi(argv[i]);
+    auto v = strtoull(argv[i], NULL, 0);
     if (f != 0) flag = f;
     if (v != 0) n = v;
   }
@@ -64,14 +70,15 @@ int parse_flag(char * argv) {
 }
 
 
-long long fibonacci(long long n) {
+cpp_int fibonacci(cpp_int n) {
   if (n <= 2) return 1;
   else return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-long long looped_fibonacci(int n) {
+cpp_int looped_fibonacci(cpp_int n) {
+  std::cout << "n: " << n << std::endl;
   if (n <= 2) return 1;
-  long long f1 = 0, f2 = 1, f = 0;
+  cpp_int f1 = 0, f2 = 1, f = 0;
   for (int i = 2; i <= n; i++) {
     f = f1 + f2;
     f1 = f2;
@@ -80,18 +87,18 @@ long long looped_fibonacci(int n) {
   return f;
 }
 
-std::map<long long, long long> cache;
-long long cached_fibonacci(long long n) {
+std::map<cpp_int, cpp_int> cache;
+cpp_int cached_fibonacci(cpp_int n) {
   if (n <= 2) return 1;
   auto it = cache.find(n);
   if (it != cache.end()) return it->second;
-  int result = cached_fibonacci(n - 1) + cached_fibonacci(n - 2);
+  cpp_int result = cached_fibonacci(n - 1) + cached_fibonacci(n - 2);
   cache[n] = result;
   return result;
 }
 
-long long matrixed_fibonacci(int n) {
-  long long F[2][2] = { 
+cpp_int matrixed_fibonacci(cpp_int n) {
+  cpp_int F[2][2] = { 
     {1,1}, 
     {1,0} 
   };
@@ -100,9 +107,9 @@ long long matrixed_fibonacci(int n) {
   return F[0][0];
 }
 
-void power(long long A[2][2], int n) {
+void power(cpp_int A[2][2], cpp_int n) {
   if (n < 2) return;
-  long long B[2][2] = {
+  cpp_int B[2][2] = {
     {1, 1},
     {1, 0}
   };
@@ -111,11 +118,11 @@ void power(long long A[2][2], int n) {
   if (n % 2 != 0) multiply(A, B);
 }
 
-void multiply(long long A[2][2], long long B[2][2]) {
-  long long a =  A[0][0] * B[0][0] + A[0][1] * B[1][0];
-  long long b =  A[0][0] * B[0][1] + A[0][1] * B[1][1];
-  long long c =  A[1][0] * B[0][0] + A[1][1] * B[1][0];
-  long long d =  A[1][0] * B[0][1] + A[1][1] * B[1][1];
+void multiply(cpp_int A[2][2], cpp_int B[2][2]) {
+  cpp_int a =  A[0][0] * B[0][0] + A[0][1] * B[1][0];
+  cpp_int b =  A[0][0] * B[0][1] + A[0][1] * B[1][1];
+  cpp_int c =  A[1][0] * B[0][0] + A[1][1] * B[1][0];
+  cpp_int d =  A[1][0] * B[0][1] + A[1][1] * B[1][1];
   A[0][0] = a;
   A[0][1] = b;
   A[1][0] = c;
