a106cfad7aacacc113e94eb2b205e31960fc90d6
Takeshi <iwatakeshi@gmail.com>
2017-10-10 21:41:18 -0600

    Update

--- CSC-482 Algorithms and Optimizations/homework/hw5-zero-triplet.cpp
+++ CSC-482 Algorithms and Optimizations/homework/hw5-zero-triplet.cpp
@@ -2,14 +2,38 @@
 #include <fstream>
 #include <vector>
 #include <sstream>
+#include <string.h>
 
-int zerotriplet(int * a, int n) {
+int zerotriplet(int * A, int n) {
   int count = 0;
   for(int i = 0; i < n; i++) {
     for (int j = i + 1; j < n; j++) {
       for (int k = j + 1; k < n; k++) {
-        if ((a[i] + a[j] + a[k]) == 0) count++;
+        if ((A[i] + A[j] + A[k]) == 0) count++;
       }
+    }
+  }
+  return count;
+}
+
+/*
+  https://en.wikipedia.org/wiki/3SUM 
+*/
+int zerotriplet_improved(int * A, int n) {
+  int count = 0;
+  for(int i = 0; i < n - 2; i++) {
+    auto a = A[i];
+    auto start = i + 1;
+    auto end = n - 1;
+    while (start < end) {
+      auto b = A[start];
+      auto c = A[end];
+      if (a + b + c == 0) {
+        count++;
+        if (b == A[start + 1]) start++;
+        else end--;
+      } else if (a + b + c > 0) end--;
+      else start++;
     }
   }
   return count;
@@ -29,10 +53,15 @@ std::vector<int> readfile(const std::string& filename){
 }
 
 int main(int argc, char * argv[]) {
-  if (argc > 1) {
-    std::cout << argv[1] << std::endl;
-    std::vector<int> v = readfile(argv[1]);
-    zerotriplet(&v[0], v.size());
+  if (argc == 3) {
+    if (strcmp(argv[1], "-n") || strcmp(argv[1], "-naive")) {
+      std::vector<int> v = readfile(argv[2]);
+      std::cout << zerotriplet(&v[0], v.size()) << std::endl;
+    } else if (strcmp(argv[1], "-i") || strcmp(argv[1], "improved")) {
+      std::vector<int> v = readfile(argv[2]);
+      std::sort(v.begin(), v.end());
+      std::cout << zerotriplet_improved(&v[0], v.size()) << std::endl;
+    }
   }
   return 0;
 }
